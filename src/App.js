ef813f5c28a8f6ec9a352a35f5592b0295297c1a
chairking95 <ohb4199@naver.com>
2023-11-12 14:01:32 +0900

    초기 컴포넌트 생성 및 CSS 처리

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className = "game">
+      <div className="game-board">
+      game-board
+      </div>
+      
+      <div className="game-info">
+      game-info
+      </div>
+    </div>
+  );
+}
+
+export default App;
