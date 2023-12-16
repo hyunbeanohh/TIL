74e00424c37ba358a27f3585142ed80dc9c393cc
chairking95 <ohb4199@naver.com>
2023-12-16 16:52:05 +0900

    클래스 형 컴포넌트 함수 형 컴포넌트로 변경 Board,Square 컴포넌트를 함수형 컴포넌트로 변경 처리

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Square.css";
 
-export default class Square extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            value : null
-        }
-    }
-    render(){
-        return(
-            <button className="square" onClick={() => {this.setState({value : 'X'})}}>
-                {this.state.value}
-            </button>
-        )
-    }
+const Square = ({onClick, value}) => {
+
+    return(
+        <button className="square" 
+        onClick={onClick}>
+            {value}
+        </button>
+    )
 }
+
+export default Square;
