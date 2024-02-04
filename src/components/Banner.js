24846c1cdb4c8c06184a4c8c3d8ad102e629311c
devbean <ohb4199@naver.com>
2024-02-04 16:07:54 +0900

    Feat : 비디오 배너 추가 Iframe에 movie key 값을 넣어 보여줄 수 있도록 처리 메인 페이지로 돌아갈 수 있는 isClicked state 추가

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import axios from '../api/axios';
 import requests from '../api/request';
 import "./Banner.css";
+import styled from 'styled-components';
 
 const Banner = () => {
 
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -29,35 +31,88 @@ const Banner = () => {
     return str?.length > length ? str.substring(0,length) + "..." : str;
   }
 
-  return (
-    <header
-      className='banner'
-      style={{
-        backgroundImage : `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition : "top center",
-        backgroundSize : "cover",
-      }}
-    >
-      <div className='banner_contents'>
-        <h1 className='banner_title'>
-          {movie.title || movie.name || movie.original_name}
-        </h1>
 
-        <div className='banner_buttons'>
-          {movie?.videos?.results[0]?.key &&
-            <button className='banner_button play'>
-              Play
-            </button>
-          }
+  if(isClicked){
+    return (
+      <>
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src = {`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width = "640"
+            height = "360"
+            frameborder = "0"
+            allow = "autoplay; fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+      <button onClick={()=>{setIsClicked(false)}}>
+        X
+      </button>
+      </>
+    )
+  }else{
+    return (
+      <header
+        className='banner'
+        style={{
+          backgroundImage : `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition : "top center",
+          backgroundSize : "cover",
+        }}
+      >
+        <div className='banner_contents'>
+          <h1 className='banner_title'>
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+  
+          <div className='banner_buttons'>
+            {movie?.videos?.results[0]?.key &&
+              <button className='banner_button play' onClick={() => {setIsClicked(true)}}>
+                Play
+              </button>
+            }
+          </div>
+          <p className='banner_description'>
+            {truncate(movie.overview,100)}
+          </p>
         </div>
-        <p className='banner_description'>
-          {truncate(movie.overview,100)}
-        </p>
-      </div>
-          <div className='banner-fadeBottom'/>
-
-    </header>
-  )
-}
+            <div className='banner-fadeBottom'/>
+  
+      </header>
+      )
+    }
+  }
 
 export default Banner
+
+const Container = styled.div`
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  flex-direction : column;
+  width : 100%;
+  height : 100vh;
+`
+
+const HomeContainer = styled.div`
+  width : 100%;
+  height : 100%;
+`
+
+const Iframe = styled.iframe`
+  width : 100%;
+  height : 100%;
+  z-index : -1;
+  opacity : 0.65;
+  border : none;
+
+  &::after {
+    content : "";
+    position : absolute;
+    top : 0;
+    left : 0;
+    width : 100%;
+    height : 100%;
+  }
+`
