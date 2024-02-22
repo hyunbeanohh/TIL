3beea35a44eca912d43be6fd1cc9fe53e9a0f420
devbean <ohb4199@naver.com>
2024-02-23 00:41:41 +0900

    Feat : 영화를 검색할 수 있는 Search 컴포넌트 기능 구현 (1차) useNavigate, useLocation 적용

--- movie-api-viewer/src/components/Nav.js
+++ movie-api-viewer/src/components/Nav.js
@@ -1,9 +1,14 @@
 import React, { useState,useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components'
 
 const Nav = () => {
 
   const [show, setShow] = useState(false);
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();  
+
   useEffect(() => {
     window.addEventListener('scroll',handelScroll)
   
@@ -19,7 +24,11 @@ const Nav = () => {
       setShow(false);
     }
   }
-
+  
+  const handleChange = (e) =>{
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }
   return (
     <NavWrapper $show={show}>
       <Logo>
@@ -29,11 +38,47 @@ const Nav = () => {
           onClick={() => (window.location.href = "/")}
           />
       </Logo>
+
+      {pathname === "/" ?
+       (<Login>Login</Login>) : 
+       <Input 
+        className='nav__input'
+        type='text'
+        placeholder='영화를 검색해주세요.'
+        value = {searchValue}
+        onChange={handleChange}
+        />}
     </NavWrapper>
   )
 }
 
 export default Nav
+
+const Login = styled.a`
+  background-color: rgba(0,0,0,0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform : translate(-50%, 0);
+  background-color : rgba(0,0,0,0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
 
 const NavWrapper = styled.nav`
   position : fixed;
