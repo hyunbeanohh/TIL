5f7db3a433450500fe2bd2d9893a2cbf9cb77ccd
devbean <ohb4199@naver.com>
2024-06-21 01:11:28 +0900

    feat : 주문 완료 페이지 구현하기 서버에 보낸 데이터를 axios.post로 받아와 response를 바탕으로 브라우저에 뿌려주기 contextAPI에 저장된 데이터를 바탕으로 axios.post 통신에 같이 보내도록 처리

--- context-api-app/react-context-app/client/src/pages/CompletePage/index.js
+++ context-api-app/react-context-app/client/src/pages/CompletePage/index.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React,{ useEffect, useContext, useState } from 'react'
+import { OrderContext } from '../../context/OrderContext'
+import axios from 'axios';
 
-const CompletePage = () => {
-  return (
-    <div>CompletePage</div>
-  )
+const CompletePage = ({setStep}) => {
+  const [loading, setLoading] = useState(true);
+  const [orderHistory, setOrderHistory] = useState([]);
+  const [orderData] = useContext(OrderContext)
+
+  useEffect(() => {
+    orderComplete(orderData);
+  }, [orderData]);
+
+  const orderComplete = async (orderData) => {
+    try {
+      const res = await axios.post('http://localhost:4000/order', orderData);
+      setOrderHistory(res.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("orderH: ", orderHistory);
+  }, [orderHistory]);
+
+  const orderTable = orderHistory.map((item) => (
+    <tr key={item.orderNumber}>
+      <td>{item.orderNumber}</td>
+      <td>{item.price}</td>
+    </tr>
+  ))
+
+  if(loading) {
+    return <h1>주문을 처리중입니다...</h1>
+  }else{
+    return (
+      <div style={{textAlign : 'center'}}>
+        <h1>주문이 성공했어요 !</h1>
+        <h2>주문 내역은 아래와 같아요.</h2>
+        
+        <table style={{margin : 'auto'}}>
+          <tbody>
+            <tr>
+              <th>주문번호</th>
+              <th>가격</th>
+            </tr>
+          {orderTable}
+          </tbody>
+        </table>
+
+        <br/>
+        <button onClick ={() => setStep(0)}>
+          처음 페이지로 돌아가시겠어요?
+        </button>
+      </div>
+    )
+  }
+
+  
 }
 
 export default CompletePage
