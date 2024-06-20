1b960947c964b1bda90214b750c9f176feb43cd9
devbean <ohb4199@naver.com>
2024-06-21 00:23:25 +0900

    feat : step state를 활용해 페이지 나누기 적용 useState를 props로 내려줘 step 상태에 따른 페이지 나누기

--- context-api-app/react-context-app/client/src/App.js
+++ context-api-app/react-context-app/client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import SummaryPage from './pages/SummaryPage'
 import OrderPage from './pages/OrderPage'
+import { useState } from 'react'
+import CompletePage from './pages/CompletePage'
 
 const App = () => {
+  const [step, setStep] = useState(0)
+
   return (
-    <div style={{ padding : '4rem' }}>
-      <OrderPage />
-      <SummaryPage />
+    <div style={{ pfirstg : '4rem' }}>
+      {step === 0 && <OrderPage setStep={setStep}/>}
+      {step === 1 && <SummaryPage setStep={setStep}/>}
+      {step === 2 && <CompletePage setStep={setStep}/>}
     </div>
   )
 }
