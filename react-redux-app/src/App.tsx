b787c21a4ceb9d5eb4054a07c8a7510a40d401be
devbean <ohb4199@naver.com>
2024-06-30 21:40:28 +0900

    Feat : Redux 라이브러리 useSelector , useDispatch 적용 - useSelector는 Redux 상태 관리 저장소에 있는 데이터를 가져올 수 있다. - useDispatch 는 Redux 상태 관리 저장소에 액션 명령을 전달한다.

--- react-redux-app/src/App.tsx
+++ react-redux-app/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducers';
 
 type Props = {
   value : any;
@@ -8,6 +10,9 @@ type Props = {
 }
 
 function App({value,onIncrement,onDecrement} : Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state:RootState) => state.counter); // RootState를 설정해주지 않으면 useSelector에서 state를 사용할 때 타입을 지정해주어야 한다.
+  const todos = useSelector((state:RootState) => state.todos);
 
   const [todoValue, settodoValue] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,18 +20,23 @@ function App({value,onIncrement,onDecrement} : Props) {
   }
   const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    dispatch({type: "ADD_TODO", text: todoValue})
     settodoValue("");
-    console.log(todoValue);
+    
   }
   return (
     <div className="App">
-      {/* Clicked : {value} times */}
+      Clicked : {counter} times
       <button onClick={onIncrement}>
         +
       </button>
       <button onClick={onDecrement}>
         -
       </button>
+
+      <ul>
+        {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+      </ul>
 
       <form onSubmit={addTodo}>
         <input type='text' value={todoValue} onChange={handleChange}/>
--- react-redux-app/src/index.tsx
+++ react-redux-app/src/index.tsx
@@ -14,11 +14,6 @@ const root = ReactDOM.createRoot(
 
 const store = createStore(rootReducer);
 
-store.dispatch({
-  type: "ADD_TODO",
-  text:"USE REDUX"
-})
-
 console.log(store.getState())
 
 const render = () => root.render(
