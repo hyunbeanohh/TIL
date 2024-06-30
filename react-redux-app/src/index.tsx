d6a700e4a9babc5c92f5933fceef3c87b2431a45
devbean <ohb4199@naver.com>
2024-06-30 21:30:45 +0900

    Feat : Redux 라이브러리 추가 , 적용 - index.tsx에 redux의 Provider 적용 - 예제 샘플 UI 및 이벤트 추가

--- react-redux-app/src/index.tsx
+++ react-redux-app/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { createStore } from 'redux';
 import counter from './reducers';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(
@@ -22,11 +23,13 @@ console.log(store.getState())
 
 const render = () => root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App 
       value = {store.getState()}
       onIncrement = {() => store.dispatch({type: "INCREMENT"})}
       onDecrement = {() => store.dispatch({type: "DECREMENT"})}
     />
+    </Provider>
   </React.StrictMode>
 );
 
