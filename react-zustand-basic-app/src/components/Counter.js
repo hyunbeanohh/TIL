d8f9f1935082b12b8b0dba42944c3ca8497a5bef
devbean <ohb4199@naver.com>
2024-07-16 00:13:13 +0900

    Zustand 톹아보기 - Counter App 간단 구현

---
+++ react-zustand-basic-app/src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useCounterStore } from '../store'
+
+const Counter = () => {
+  const { count, increment, reset, setNumber} = useCounterStore();
+  return (
+    <div>
+        <p>{count}</p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={()=> setNumber(3)}>3</button>
+    </div>
+  )
+}
+export default Counter
