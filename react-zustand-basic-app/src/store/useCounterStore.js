bc38d6c80b5654fb4683e9affe9f875c80e257ca
devbean <ohb4199@naver.com>
2024-07-18 00:34:48 +0900

    Zustand 톹아보기(5) - Persist,devtools 적용된 구문 가독성 높이기

--- react-zustand-basic-app/src/store/useCounterStore.js
+++ react-zustand-basic-app/src/store/useCounterStore.js
@@ -1,13 +1,30 @@
 import create from 'zustand';
-import {persist} from 'zustand/middleware';
-export const useCounterStore = create(
-    persist(
-        (set) => ({
-        count : 1,
-        increment: () => set((state) => ({count: state.count + 1})),
-        reset: () => set({count: 0}),
-        setNumber: (number) => set({count: number})
-        }),
-        {name : 'counter'}
-    )
-);
+import { persist,devtools } from 'zustand/middleware';
+// export const useCounterStore = create(
+//     persist(
+//         devtools(
+//             (set) => ({
+//             count : 1,
+//             increment: () => set((state) => ({count: state.count + 1})),
+//             reset: () => set({count: 0}),
+//             setNumber: (number) => set({count: number})
+//             }),
+//             {name : 'counter'}
+//         ),{
+//             name : 'counter-storage',
+//             getStorage: () => sessionStorage
+//         }
+//     )
+// );
+
+let counterStore = (set) => ({
+    count : 1,
+    increment: () => set((state) => ({count: state.count + 1})),
+    reset: () => set({count: 0}),
+    setNumber: (number) => set({count: number})
+})
+
+counterStore = devtools(counterStore);
+counterStore = persist (counterStore, {name : 'counter'});
+
+export const useCounterStore = create(counterStore);
