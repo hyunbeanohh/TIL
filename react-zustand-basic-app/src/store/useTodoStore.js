29f8ada7bff008e8c4e7bfb74cf1e35b49f3c2c4
devbean <ohb4199@naver.com>
2024-07-22 23:07:33 +0900

    Zustand 톹아보기(6) - Persist,devtools 적용된 구문 가독성 높이기(2)

--- react-zustand-basic-app/src/store/useTodoStore.js
+++ react-zustand-basic-app/src/store/useTodoStore.js
@@ -1,46 +1,45 @@
 import {create} from 'zustand'
-import {persist} from 'zustand/middleware'
+import {devtools, persist} from 'zustand/middleware'
 
-export const useTodoStore = create(
-    persist(
-        (set) => ({
-        todos: [],
-        addTodo : (todoText) =>
-            set( (state) =>({
-                todos:[
-                    ...state.todos,
-                    {
-                        text: todoText,
-                        id: getId(),
-                        isCompleted : false
-                    }
-                ]
-            })),
-        deleteTodo: (todoId) =>
-                set( (state) => ({
-                    todos: state.todos.filter((todo) => todo.id !== todoId)
-                })),
-        completeTodo : (todoId) =>
+let todoStore = (set) => ({
+    todos: [],
+    addTodo : (todoText) =>
+        set( (state) =>({
+            todos:[
+                ...state.todos,
+                {
+                    text: todoText,
+                    id: getId(),
+                    isCompleted : false
+                }
+            ]
+        })),
+    deleteTodo: (todoId) =>
             set( (state) => ({
-                todos : state.todos.map((todo) => {
-                    if(todo.id === todoId){
-                        return {
-                            ...todo,
-                            isCompleted: true
-                        }
+                todos: state.todos.filter((todo) => todo.id !== todoId)
+            })),
+    completeTodo : (todoId) =>
+        set( (state) => ({
+            todos : state.todos.map((todo) => {
+                if(todo.id === todoId){
+                    return {
+                        ...todo,
+                        isCompleted: true
                     }
-                    return todo
-                })
-            }))
-        }),
-        {
-            name : 'todo-app',
-            getStorage: () => sessionStorage 
-            // session storage에 저장 -> 브라우저를 닫으면 데이터가 사리진다.
-        }
-    )
-)
+                }
+                return todo
+            })
+        }))
+    })
 
+todoStore = devtools(todoStore);
+todoStore = persist(todoStore,{
+    name : 'todo-app',
+    getStorage: () => sessionStorage 
+    // session storage에 저장 -> 브라우저를 닫으면 데이터가 사리진다.
+});
+
+export const useTodoStore = create(todoStore);
 
 let id = 0;
 function getId(){
